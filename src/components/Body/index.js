b843d3c06ad0e74ef24e5cd0604801f0200de7c1
Poornima-Ranguri <poornimaranguri@gmail.com>
2024-10-16 13:53:04 +0000

    Layout

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -7,45 +7,42 @@ const Body = () => (
     {value => {
       const {showContent, showLeftNavbar, showRightNavbar} = value
 
-      const showContentClassName = showContent ? 'content-card' : 'display-none'
-
-      const showLeftNavbarClass = showLeftNavbar
-        ? 'left-nav-menu'
-        : 'display-none'
-
-      const showRightNavbarClass = showRightNavbar
-        ? 'left-nav-menu'
-        : 'display-none'
-
       return (
         <div className="body-card">
-          <div className={showLeftNavbarClass}>
-            <h1 className="body-headings">Left Navbar Menu</h1>
-            <ul className="items-list">
-              <li className="item">item 1</li>
-              <li className="item">item 2</li>
-              <li className="item">item 3</li>
-              <li className="item">item 4</li>
-            </ul>
-          </div>
-          <div className={showContentClassName}>
-            <h1 className="body-headings">Content</h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea
-            </p>
-          </div>
-          <div className={showRightNavbarClass}>
-            <h1 className="body-headings">Right Navbar</h1>
-            <div className="Ad">
-              <p>Ad 1</p>
+          {showLeftNavbar && (
+            <div className="content-card">
+              <h1 className="body-headings">Left Navbar Menu</h1>
+              <ul className="items-list">
+                <li className="item">Item 1</li>
+                <li className="item">Item 2</li>
+                <li className="item">Item 3</li>
+                <li className="item">Item 4</li>
+              </ul>
             </div>
-            <div className="Ad">
-              <p>Ad 2</p>
+          )}
+
+          {showContent && (
+            <div className="left-nav-menu">
+              <h1 className="body-headings">Content</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea
+              </p>
             </div>
-          </div>
+          )}
+          {showRightNavbar && (
+            <div className="left-nav-menu">
+              <h1 className="body-headings">Right Navbar</h1>
+              <div className="Ad">
+                <p>Ad 1</p>
+              </div>
+              <div className="Ad">
+                <p>Ad 2</p>
+              </div>
+            </div>
+          )}
         </div>
       )
     }}
